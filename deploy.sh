167f904561247f4a86331977d13a36c5b4cd82e4
Madushan-Jaya-Sri <111506676+Madushan-Jaya-Sri@users.noreply.github.com>
2024-10-28 01:10:18 +0530

    change the location

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,10 @@ sudo chkconfig docker on
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+
 # Create nginx directory if it doesn't exist
 mkdir -p nginx
+sudo cp ~/app/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # Pull the latest code and deploy
 git pull origin main
