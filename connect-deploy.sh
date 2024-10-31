0eec6dd451d9694f07063b5e380d3f8615e0189d
Madushan-Jaya-Sri <111506676+Madushan-Jaya-Sri@users.noreply.github.com>
2024-10-31 15:22:17 +0530

    updated

--- connect-deploy.sh
+++ connect-deploy.sh
@@ -35,7 +35,7 @@ server {
     server_name 13.202.203.220;
 
     location / {
-        proxy_pass http://localhost:5000;
+        proxy_pass http://app-web-1:5001;;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
